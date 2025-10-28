52f94b3f6a7fade295764825e8c0079f0dab1926
JMJ <ghtntkdnsk@naver.com>
2025-10-29 03:23:45 +0900

    refactor(LottoMachineController): LottoPurchaseAmount 객체 생성에 따른 변경

--- src/main/java/lotto/controller/LottoMachineController.java
+++ src/main/java/lotto/controller/LottoMachineController.java
@@ -1,5 +1,9 @@
 package lotto.controller;
 
+import java.util.List;
+import lotto.domain.lotto.Lotto;
+import lotto.domain.lottoMachine.LottoMachine;
+import lotto.domain.lottoMachine.LottoPurchaseAmount;
 import lotto.view.InputView;
 
 public class LottoMachineController {
@@ -12,10 +16,16 @@ public class LottoMachineController {
     public void run() {
         while (true) {
             try {
-                String purchaseAmount = inputView.inputLottoPurchasePrice();
+                List<Lotto> purchasedLottos = purchaseLottos();
             } catch (IllegalArgumentException exception) {
                 System.out.println(exception.getMessage());
             }
         }
+    }
+
+    private List<Lotto> purchaseLottos() {
+        LottoPurchaseAmount purchaseAmount = LottoPurchaseAmount.from(inputView.inputLottoPurchasePrice());
+        LottoMachine lottoMachine = new LottoMachine(purchaseAmount);
+        return lottoMachine.generateLottos();
     }
 }
