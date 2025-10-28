1f2e569aa151d204deeb29706a5e4d88343d9b25
JMJ <ghtntkdnsk@naver.com>
2025-10-28 22:39:19 +0900

    test: 구입 금액을 기준으로 발행할 로또의 수량 계산 테스트 구현

--- src/test/java/lotto/domain/LottoMachineTest.java
+++ src/test/java/lotto/domain/LottoMachineTest.java
@@ -1,4 +1,22 @@
 package lotto.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import lotto.Lotto;
+import org.junit.jupiter.api.Test;
+
 public class LottoMachineTest {
+    @Test
+    void 구입_금액을_기준으로_발행할_로또의_수량을_계산한다() {
+        //given
+        int lottoPurchasePrice = 8000;
+
+        //when
+        LottoMachine lottoMachine = new LottoMachine(lottoPurchasePrice);
+        List<Lotto> lottos = lottoMachine.generateLottos();
+
+        //then
+        assertThat(lottos).hasSize(8);
+    }
 }
