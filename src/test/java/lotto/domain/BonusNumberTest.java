c89d17cb89a592c4cbe7fdbc97f705932c4e9600
JMJ <ghtntkdnsk@naver.com>
2025-10-30 03:55:57 +0900

    test(BonusNumberTest): 보너스 번호를 담당하는 BonusNumber 테스트 추가

--- src/test/java/lotto/domain/BonusNumberTest.java
+++ src/test/java/lotto/domain/BonusNumberTest.java
@@ -1,4 +1,39 @@
 package lotto.domain;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
+import lotto.exception.ErrorMessage;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 public class BonusNumberTest {
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, -1, 46, 100})
+    void 보너스_번호는_1부터_45_사이여야_한다(int input) {
+        assertThatThrownBy(() -> new BonusNumber(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.BONUS_NUMBER_SHOULD_BE_BETWEEN_1_AND_45.getMessage());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"7,8", "7, 8", "1,2,3",})
+    void 보너스_번호가_여러개면_예외_처리한다(String input) {
+        assertThatThrownBy(() -> Parser.parseBonusNumber(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.BONUS_NUMBER_SHOULD_BE_SINGLE_NUMBER.getMessage());
+    }
+
+    @Test
+    void 당첨_번호에_있는_숫자를_보너스_번호로_입력하면_예외_처리한다() {
+        //given
+        WinningNumbers winningNumbers = new WinningNumbers(List.of(1, 2, 3, 4, 5, 6));
+
+        //when & then
+        assertThatThrownBy(() -> new BonusNumber(1, List.of(1, 2, 3, 4, 5, 6)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.BONUS_NUMBER_SHOULD_NOT_DUPLICATE_WINNING_NUMBERS.getMessage());
+    }
 }
