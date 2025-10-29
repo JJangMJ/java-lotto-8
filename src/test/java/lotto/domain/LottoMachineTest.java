2b7b8bfad0b2d772b88dcb3f06db6dcd78cd2d0b
JMJ <ghtntkdnsk@naver.com>
2025-10-29 18:18:54 +0900

    refactor(LottoMachineTest): RandomLottoNumberGenerator 객체 생성에 따른 테스트 코드 변경

--- src/test/java/lotto/domain/LottoMachineTest.java
+++ src/test/java/lotto/domain/LottoMachineTest.java
@@ -10,9 +10,10 @@ class LottoMachineTest {
     void 구입_금액을_기준으로_발행할_로또의_수량을_계산한다() {
         //given
         LottoPurchaseAmount purchaseAmount = LottoPurchaseAmount.from("8000");
+        RandomLottoNumberGenerator lottoNumberGenerator = new RandomLottoNumberGenerator();
 
         //when
-        LottoMachine lottoMachine = new LottoMachine(purchaseAmount);
+        LottoMachine lottoMachine = new LottoMachine(purchaseAmount, lottoNumberGenerator);
         List<Lotto> lottos = lottoMachine.generateLottos();
 
         //then
