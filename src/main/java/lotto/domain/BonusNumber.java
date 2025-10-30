0d13d3de806067554199a9b0c5864c6478588734
JMJ <ghtntkdnsk@naver.com>
2025-10-30 18:01:37 +0900

    feat(BonusNumber): 보너스 번호를 담당하는 BonusNumber 객체 생성

---
+++ src/main/java/lotto/domain/BonusNumber.java
@@ -0,0 +1,29 @@
+package lotto.domain;
+
+import java.util.List;
+import lotto.exception.ErrorMessage;
+
+public class BonusNumber {
+    private static final int LOTTO_MIN_NUMBER = 1;
+    private static final int LOTTO_MAX_NUMBER = 45;
+    private final int number;
+
+    public BonusNumber(int number, List<Integer> winningNumbers) {
+        validateNumberRange(number);
+        validateNotDuplicateWith(winningNumbers);
+        this.number = number;
+    }
+
+    private void validateNumberRange(int number) {
+        if (number < LOTTO_MIN_NUMBER || number > LOTTO_MAX_NUMBER) {
+            throw new IllegalArgumentException(ErrorMessage.BONUS_NUMBER_SHOULD_BE_BETWEEN_1_AND_45.getMessage());
+        }
+    }
+
+    private void validateNotDuplicateWith(List<Integer> winningNumbers) {
+        if (winningNumbers.contains(number)) {
+            throw new IllegalArgumentException(
+                    ErrorMessage.BONUS_NUMBER_SHOULD_NOT_DUPLICATE_WINNING_NUMBERS.getMessage());
+        }
+    }
+}
--- src/main/java/lotto/exception/ErrorMessage.java
+++ src/main/java/lotto/exception/ErrorMessage.java
@@ -9,7 +9,10 @@ public enum ErrorMessage {
     WINNING_NUMBERS_SHOULD_BE_NUMBER("당첨 번호는 숫자로만 이루어져야 합니다."),
     WINNING_NUMBERS_SHOULD_BE_SIX_NUMBERS("당첨 번호는 6개의 숫자로 이루어져야 합니다."),
     WINNING_NUMBERS_SHOULD_BE_BETWEEN_1_AND_45("당첨 번호는 1~45 사이의 숫자만 입력할 수 있습니다."),
-    WINNING_NUMBERS_CANNOT_BE_DUPLICATED("당첨 번호는 중복될 수 없습니다.");
+    WINNING_NUMBERS_CANNOT_BE_DUPLICATED("당첨 번호는 중복될 수 없습니다."),
+    BONUS_NUMBER_SHOULD_BE_SINGLE_NUMBER("보너스 번호는 하나만 입력해야 합니다."),
+    BONUS_NUMBER_SHOULD_BE_BETWEEN_1_AND_45("보너스 번호는 1~45 사이의 숫자만 입력할 수 있습니다."),
+    BONUS_NUMBER_SHOULD_NOT_DUPLICATE_WINNING_NUMBERS("보너스 번호는 당첨 번호에 없는 숫자를 입력해야 합니다.");
 
     private static final String ERROR_MESSAGE_HEADER = "[ERROR] ";
     private final String errorMessage;
