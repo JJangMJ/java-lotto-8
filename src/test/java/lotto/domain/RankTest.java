47d7d747d1a5f38be814ea0aca26e58a6b5d29ce
JMJ <ghtntkdnsk@naver.com>
2025-10-31 04:08:42 +0900

    test(RankTest): 일치하는 번호 개수와 보너스 번호 일치 여부에 따른 등수를 판별하는 RankTest 추가

---
+++ src/test/java/lotto/domain/RankTest.java
@@ -0,0 +1,27 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class RankTest {
+    @ParameterizedTest(name = "[{index}] match={0}, bonus={1} ⇒ {2}")
+    @CsvSource({
+            "6, false, FIRST",
+            "6, true,  FIRST",
+            "5, true,  SECOND",
+            "5, false, THIRD",
+            "4, false, FOURTH",
+            "4, true,  FOURTH",
+            "3, false, FIFTH",
+            "3, true,  FIFTH",
+            "2, false, NOTHING",
+            "2, true,  NOTHING",
+            "1, false, NOTHING",
+            "0, false, NOTHING"
+    })
+    void 일치하는_번호_개수와_보너스_번호_일치_여부에_따라_1등부터_5등까지의_등수_중_하나로_결정한다(int matchCount, boolean hasBonus, Rank expected) {
+        assertThat(Rank.calculateRank(matchCount, hasBonus)).isEqualTo(expected);
+    }
+}
