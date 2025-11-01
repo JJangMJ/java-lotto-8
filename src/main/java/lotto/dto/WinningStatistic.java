65528843a31d66f7b60cd14add622e29d5c8b88c
JMJ <ghtntkdnsk@naver.com>
2025-11-01 21:02:55 +0900

    feat(WinningStatistic): 당첨 통계를 관리하는 WinningStatistic 클래스 생성

--- src/main/java/lotto/dto/WinningStatistic.java
+++ src/main/java/lotto/dto/WinningStatistic.java
@@ -1,6 +1,7 @@
-package lotto.domain;
+package lotto.dto;
 
 import java.util.Map;
+import lotto.domain.Rank;
 
 public record WinningStatistic(Map<Rank, Integer> rankResults, double profitRate) {
 }
